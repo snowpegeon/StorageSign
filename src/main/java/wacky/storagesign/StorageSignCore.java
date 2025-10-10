f7cdac7b278a24b739145a4a2de780795269af3a
SaidRed <said.red018@gmail.com>
2025-10-10 18:10:34 +0900

    paper AutoExportバグ回避用プログラム

--- src/main/java/wacky/storagesign/StorageSignCore.java
+++ src/main/java/wacky/storagesign/StorageSignCore.java
@@ -61,6 +61,7 @@ import org.bukkit.inventory.meta.PotionMeta;
 import org.bukkit.inventory.recipe.CraftingBookCategory;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.potion.PotionType;
+import org.bukkit.scheduler.BukkitRunnable;
 import wacky.storagesign.client.paper.PaperSSEvent;
 import wacky.storagesign.client.spigot.SpigotSSEvent;
 import wacky.storagesign.config.StorageSignNBTConfig;
@@ -778,9 +779,8 @@ public class StorageSignCore extends JavaPlugin implements Listener {
         logger.debug("This Event is Temp Inventory.");
       } else if (event.getSource().getHolder() instanceof Minecart){
         logger.debug("This Event is Minecart.");
-      } else if (event.getSource().getHolder() instanceof DoubleChest) {
+      } else if (event.getSource().getHolder() instanceof DoubleChest lc) {
         logger.debug("This Event is DoubleChest.");
-        DoubleChest lc = (DoubleChest) event.getSource().getHolder();
         blockInventory[0] = (BlockState) lc.getLeftSide();
         blockInventory[1] = (BlockState) lc.getRightSide();
       } else if (!(event.getSource().getHolder() instanceof BlockState)){
@@ -841,7 +841,7 @@ public class StorageSignCore extends JavaPlugin implements Listener {
       }
 			if (flag) {
         logger.debug("Export Sign.");
-				exportSign(sign, storageSign, item, event.getSource(), event.getDestination());
+				new exportSign(sign, storageSign, item.clone(), event.getSource()).runTask(this);
 			}
     }
     logger.debug("★onItemMove:End.");
@@ -864,182 +864,47 @@ public class StorageSignCore extends JavaPlugin implements Listener {
     sign.update();
   }
 
-  //搬出先ブロックに枠指定があると事故る
-  private void exportSign(Sign sign, StorageSign storageSign, ItemStack item, Inventory inv,
-      Inventory dest) {
-    logger.debug("exportSign:Start.");
-    logger.trace("item: " + item);
-    logger.trace("!inv.containsAtLeast(item, item.getMaxStackSize(): " + !inv.containsAtLeast(item, item.getMaxStackSize()));
-    logger.trace("storageSign.getAmount(): " + storageSign.getAmount());
-    logger.trace("item.getAmount(): " + item.getAmount());
-    if (!inv.containsAtLeast(item, item.getMaxStackSize())
-        && storageSign.getAmount() >= item.getAmount()) {
-      int stacks = 0;
-      int amount = 0;
-      ItemStack[] contents = dest.getContents();
+  public class exportSign extends BukkitRunnable {
 
-      logger.trace("dest.getType(): " + dest.getType());
-      logger.trace("item.getType(): " + item.getType());
-      // TODO 1.21でアイテム追加。
-      if (dest.getType() == InventoryType.BREWING) {
-        switch (item.getType()) {
-          case NETHER_WART://上
-          case SUGAR:
-          case REDSTONE:
-          case GLOWSTONE_DUST:
-          case GUNPOWDER:
-          case RABBIT_FOOT:
-          case GLISTERING_MELON_SLICE:
-          case GOLDEN_CARROT:
-          case MAGMA_CREAM:
-          case GHAST_TEAR:
-          case SPIDER_EYE:
-          case FERMENTED_SPIDER_EYE:
-          case DRAGON_BREATH:
-          case PUFFERFISH:
-          case TURTLE_HELMET:
-          case PHANTOM_MEMBRANE:
-            //上から搬入
-            logger.debug("This item is PHANTOM_MEMBRANE.");
-            logger.trace("inv.getLocation().getBlockY()" + inv.getLocation().getBlockY());
-            logger.trace("dest.getLocation().getBlockY()" + dest.getLocation().getBlockY());
-            logger.trace("inv.getLocation().getBlockY() > dest.getLocation().getBlockY()" + (inv.getLocation().getBlockY() > dest.getLocation().getBlockY()));
-            logger.trace("contents[3]: " + contents[3]);
-						if (inv.getLocation().getBlockY() > dest.getLocation().getBlockY()) {
-              logger.debug("This item import at up.");
-							if (contents[3] != null && !item.isSimilar(contents[3])) {
-                //他のアイテムが詰まってる
-                logger.debug("This item is clog.");
-								return;
-							} else {
-                logger.debug("This item Export.");
-								break;
-							}
-						} else {
-              logger.debug("This item import not.");
-							return;
-						}
+    Sign SSSign;
+    StorageSign SS;
+    ItemStack moveItem;
+    Inventory inventory;
 
-          case BLAZE_POWDER:
-            //横or上
-            logger.debug("This item is BLAZE_POWDER.");
-            logger.trace("inv.getLocation().getBlockY()" + inv.getLocation().getBlockY());
-            logger.trace("dest.getLocation().getBlockY()" + dest.getLocation().getBlockY());
-            logger.trace("inv.getLocation().getBlockY() > dest.getLocation().getBlockY()" + (inv.getLocation().getBlockY() > dest.getLocation().getBlockY()));
-            logger.trace("inv.getLocation().getBlockY() == dest.getLocation().getBlockY(): " + (inv.getLocation().getBlockY() == dest.getLocation().getBlockY()));
-            if (inv.getLocation().getBlockY() > dest.getLocation().getBlockY()) {//上から搬入
-              logger.debug("This item import at up.");
-              logger.trace("contents[3]: " + contents[3]);
-							if (contents[3] != null && !item.isSimilar(contents[3])) {
-                //他のアイテムが詰まってる
-                logger.debug("This item is clog.");
-								return;
-							} else {
-                logger.debug("This item import not.");
-								break;
-							}
-            } else if (inv.getLocation().getBlockY() == dest.getLocation().getBlockY()) {//横
-              logger.debug("This item import at beside.");
-              logger.trace("contents[4]: " + contents[4]);
-							if (contents[4] != null && contents[4].getAmount() == 64) {
-                //パウダー詰まり
-                logger.debug("This item is clog.");
-								return;
-							} else {
-                logger.debug("This item Export.");
-								break;
-							}
-						} else {
-              logger.debug("This item import not.");
-							return;
-						}
-
-          case POTION:
-          case SPLASH_POTION:
-          case LINGERING_POTION:
-            //横or下
-            logger.debug("This item is POTIONS.");
-            logger.trace("inv.getLocation().getBlockY()" + inv.getLocation().getBlockY());
-            logger.trace("dest.getLocation().getBlockY()" + dest.getLocation().getBlockY());
-            logger.trace("inv.getLocation().getBlockY() <= dest.getLocation().getBlockY()" + (inv.getLocation().getBlockY() <= dest.getLocation().getBlockY()));
-						if (inv.getLocation().getBlockY() <= dest.getLocation().getBlockY()) {
-              logger.debug("This item import at beside or down.");
-              logger.trace("contents[0]: " + contents[0]);
-              logger.trace("contents[1]: " + contents[1]);
-              logger.trace("contents[2]: " + contents[2]);
-							if (contents[0] != null && contents[1] != null && contents[2] != null) {
-                logger.debug("This item is clog.");
-								return;
-							} else {
-                logger.debug("This item Export.");
-								break;
-							}
-						} else {
-              logger.debug("This item import not.");
-							return;
-						}
-          default://ロスト回避
-            logger.debug("This Item not support BREW Item.");
-            return;
-        }
-      } else if (dest.getType() == InventoryType.FURNACE
-          || dest.getType() == InventoryType.BLAST_FURNACE
-          || dest.getType() == InventoryType.SMOKER) {
-        logger.debug("This Type is Furnace Series.");
-        logger.trace("inv.getLocation().getBlockY()" + inv.getLocation().getBlockY());
-        logger.trace("dest.getLocation().getBlockY()" + dest.getLocation().getBlockY());
-        logger.trace("inv.getLocation().getBlockY() > dest.getLocation().getBlockY(): " + (inv.getLocation().getBlockY() > dest.getLocation().getBlockY()));
-        if (inv.getLocation().getBlockY() > dest.getLocation().getBlockY()) {//上から搬入
-          logger.debug("This item import at up.");
-          logger.trace("contents[0]: " + contents[0]);
-					if (contents[0] != null && !item.isSimilar(contents[0])) {
-            //他のアイテムが詰まってる
-            logger.debug("This item is clog.");
-						return;
-					}
-        } else {
-          //横から(下から)
-          logger.debug("This item import at beside or down.");
-          logger.trace("contents[1]: " + contents[1]);
-					if (!item.getType().isFuel() || contents[1] != null && !item.isSimilar(contents[1])) {
-            //燃料以外 or 他のアイテムが詰まってる
-            logger.debug("This item is clog or not Fuel.");
-						return;
-					}
-        }
-      }
-
-      logger.debug("Export Item");
-      //PANPANによるロスト回避
-      for (int i = 0; i < contents.length; i++) {
-        logger.trace("i: " + i);
-        logger.trace("contents[i]: " + contents[i]);
-        if (item.isSimilar(contents[i])) {
-          stacks++;
-          amount += contents[i].getAmount();
-          logger.trace(" stacks: " + stacks);
-          logger.trace(" amount: " + amount);
-        }
-      }
-      logger.trace("amount == stacks * item.getMaxStackSize(): " + (amount
-          == stacks * item.getMaxStackSize()));
-      logger.trace("dest.firstEmpty() == -1: " + (dest.firstEmpty() == -1));
-      if (amount == stacks * item.getMaxStackSize() && dest.firstEmpty() == -1) {
-        logger.debug("Item less than Stack.not Export.");
-        return;
-      }
-
-      logger.debug("Export Item to Inventory.");
-      ItemStack cItem = item.clone();
-      inv.addItem(cItem);
-      storageSign.addAmount(-cItem.getAmount());
+    exportSign(Sign sign, StorageSign storageSign, ItemStack moveItemStack, Inventory sourceInventory){
+      logger.debug("exportSign:Start.");
+      logger.trace("moveItem: " + moveItemStack);
+      logger.trace("storageSign.getAmount(): " + storageSign.getAmount());
+      logger.trace("moveItem.getAmount(): " + moveItemStack.getAmount());
+      logger.trace("sourceInventory.getLocation(): " + sourceInventory.getLocation());
+      SSSign = sign;
+      SS = storageSign;
+      moveItem = moveItemStack;
+      inventory = sourceInventory;
     }
-    for (int i = 0; i < 4; i++) {
-      logger.trace("set Line i:" + i + ". Text: " + storageSign.getSigntext(i));
-      sign.getSide(Side.FRONT).setLine(i, storageSign.getSigntext(i));
+    /**
+     * Runs this operation.
+     */
+    @Override
+    public void run() {
+      logger.debug("exportSign:Task Start.");
+      logger.trace("!inv.containsAtLeast(item, item.getMaxStackSize(): " +
+          !inventory.containsAtLeast(moveItem, moveItem.getMaxStackSize()));
+      if (!inventory.containsAtLeast(moveItem, moveItem.getMaxStackSize())
+          && SS.getAmount() >= moveItem.getAmount()){
+
+        logger.trace("moveItem.getType(): " + moveItem.getType());
+        logger.debug("Export Item to Inventory.");
+        inventory.addItem(moveItem);
+        SS.addAmount(-moveItem.getAmount());
+      }
+      for (int i = 0; i < 4; i++) {
+        logger.trace("set Line i:" + i + ". Text: " + SS.getSigntext(i));
+        SSSign.getSide(Side.FRONT).setLine(i, SS.getSigntext(i));
+      }
+      SSSign.update();
+      logger.debug("ExportSign:End");
     }
-    sign.update();
-    logger.debug("ExportSign:End");
   }
 
   @EventHandler
